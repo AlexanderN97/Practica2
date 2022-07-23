6eb423723ff6da0d331584f3143d3f03d84c268f
Alexander Nascimento <15-11002@usb.ve>
2022-07-23 17:12:07 -0400

    Haciendo commit de la funcion del punto 3 y del .gitignore

--- Practica2.py
+++ Practica2.py
@@ -1,13 +1,18 @@
+#Imports
+
 import pprint
 from pprint import pp
 import json
 from urllib import response
 import requests
 import csv
-import os
 
-urlo = "https://api.meraki.com/api/v1/organizations"
+#Urls
+urlorg = "https://api.meraki.com/api/v1/organizations"
+urldev= "https://api.meraki.com/api/v1/organizations/organizationId/devices"
+urlsta= "https://api.meraki.com/api/v0/organizations/organizationId/deviceStatuses"
 
+#Header
 payload = None
 headers = {
     "Content-Type": "application/json",
@@ -15,9 +20,102 @@ headers = {
     "X-Cisco-Meraki-API-Key": "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
 }
 
-response = requests.request('GET', urlo, headers=headers, data = payload)
-orgData= json.loads(response.text)
+#Functions
+
+def orgList():                                  # Obtiene la lista de la API_KEY
+    response = requests.request('GET', urlorg, headers=headers, data = payload)
+    orgList= json.loads(response.text)
+    return orgList
+
+def  orgId(orgList):                                    #Obtiene el Id asociado al nombre de organización que se le pasa
+    DeLab=[]
+    for i in range(len(orgList)):
+        if(orgList[i]["name"]=="DeLab"):    	    
+            DeLab.append(orgList[i])
+        DeLab.append(0)
+    return DeLab[0]["id"]
+
+def orgDev(Id):                                             #Obtiene los devices de DeLab
+    urldev1="https://api.meraki.com/api/v1/organizations/organizationId/devices".replace('organizationId', Id)
+    response= requests.request('GET', urldev1, headers=headers, data = payload)
+    orgDev= json.loads(response.text)
+    return orgDev
+
+def orgSta(Id):                                             #Obtiene los status de los devices de DeLab
+    urlsta1="https://api.meraki.com/api/v0/organizations/organizationId/deviceStatuses".replace('organizationId', Id)
+    response= requests.request('GET', urlsta1, headers=headers, data = payload)
+    orgStatus=response.json()
+    return orgStatus
 
 
-pp(orgData)
+def productType(orgDev):                                    # Simplificamos la lista para que nos de los devices que son tipo appliance y wireless
+    simplifiedList=[]
+    for i in range(len(orgDev)):
+        if(orgDev[i]["productType"]== "appliance"):
+            simplifiedList.append(orgDev[i])
+        if(orgDev[i]["productType"]== "wireless"):
+            simplifiedList.append(orgDev[i])
+    return simplifiedList
 
+def reorderList(x,y):                                       # Arreglamos las listas y les damos el orden establecido en la actividad y borramos los datos innecesarios
+    for i in range(len(x)):
+        xcopy = x[i].copy()
+        x[i].clear()
+        
+        if xcopy["productType"]=="wireless":
+            x[i]["productType"] = xcopy["productType"]
+            x[i]["model"] = xcopy["model"]
+            x[i]["name"] = xcopy["name"]
+            x[i]["mac"] = xcopy["mac"]
+            for k in range (len(y)):
+                if x[i]["mac"] == y[k]["mac"]:
+                    x[i]["publicIp"] = y[k]["publicIp"]
+            x[i]["lanIp"] = xcopy["lanIp"]
+            x[i]["serial"] = xcopy["serial"]
+            for j in range (len(y)):
+                if x[i]["serial"] == y[j]["serial"]:
+                    x[i]["status"] = y[j]["status"]
+            
+                           
+        else:
+            x[i]["productType"] = xcopy["productType"]
+            x[i]["model"] = xcopy["model"]
+            x[i]["name"] = xcopy["name"]
+            x[i]["mac"] = xcopy["mac"]
+            for k in range (len(y)):
+                if x[i]["mac"] == y[k]["mac"]:
+                    x[i]["publicIp"] = y[k]["publicIp"]
+            x[i]["wan1Ip"] = xcopy["wan1Ip"]
+            x[i]["serial"] = xcopy["serial"]
+            for j in range (len(y)):
+                if x[i]["serial"] == y[j]["serial"]:
+                    x[i]["status"] = y[j]["status"]
+
+def jsontocsv(x):                                           #Pasamos de json a csv
+    blank=[]
+    header=x[0].keys()
+    with open('DevicesList.csv','w') as inventory:
+        inventory_writer= csv.writer(inventory,delimiter=',')
+        inventory_writer.writerow(header)
+        for i in range(len(x)):
+            if(x[0]['productType']==x[i]['productType']):
+                inventory_writer.writerow(x[i].values())
+        i=0
+        while(x[0]['productType']==x[i]['productType']):
+            i=1+i
+        header=x[i].keys()
+        inventory_writer.writerow(blank)
+        inventory_writer.writerow(header)
+        for i in range(len(x)):
+            if(x[0]['productType']!=x[i]['productType']):
+                inventory_writer.writerow(x[i].values())
+
+
+
+list=orgList()
+id=orgId(list)
+devices=orgDev(id)
+devicesStatuses=orgSta(id)
+devicesList=productType(devices)
+reorderList(devicesList,devicesStatuses) #Se reordena devicesList
+jsontocsv(devicesList)
